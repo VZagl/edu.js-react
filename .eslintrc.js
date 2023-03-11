7e72971e02952ca839140c1245aec756ad8d5773
VZagl <waah777@gmail.com>
2023-03-11 15:47:17 +0200

    configs

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'es2021': true
+	},
+	
+	'extends': [
+		'eslint:recommended',
+		'plugin:react/recommended'
+	],
+	
+	'overrides': [
+	],
+	'parserOptions': {
+		'ecmaVersion': 'latest',
+		'sourceType': 'module'
+	},
+	'plugins': [
+		'react'
+	],
+	'rules': {
+		'indent': [
+			'warn',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'windows'
+		],
+		'quotes': [
+			'warn',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		],
+		'no-unused-vars': [
+			'warn'
+			//, { 'vars': 'all', 'args': 'after-used', 'ignoreRestSiblings': false }
+		]
+	}
+};
