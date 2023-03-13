30ca69b88ed65a38c85866b65e46379d5ca9e65e
Zahliada VM <waah777@gmail.com>
2023-03-13 16:36:42 +0200

    самостоятельная работа

---
+++ 019/js/script.js
@@ -0,0 +1,23 @@
+console.log('=> js/script.js');
+'use strict';
+
+let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '0');
+console.log(`просмотрели ${numberOfFilms} фильмов.`);
+
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
+};
+
+const countFilms = 2;
+for ( let i=0; i<countFilms; i++ ) {
+	let lastFilmName = prompt('Один из последних просмотренных фильмов?', '');
+	let lastFilmRate = prompt('На сколько оцените его?', '0');
+	personalMovieDB.movies[lastFilmName] = lastFilmRate;
+}
+
+console.log('personalMovieDB=');
+console.log(personalMovieDB);
