02cc966c24c9fe80c1a302072d1bf58ee25a813c
Zahliada VM <waah777@gmail.com>
2023-03-15 14:44:44 +0200

    023_code_3 end

---
+++ 023/023_code_3.js
@@ -0,0 +1,76 @@
+// Место для первой задачи
+//function firstTask() {
+for (let i=5;i<=10;i++) {
+	console.log(i);
+}
+			
+    
+//}
+
+// Место для второй задачи
+//function secondTask() {
+for (let i=20; i>=10; i--) {
+	if (i===13) break;
+	console.log(i);
+}
+			
+    
+//}
+
+// Место для третьей задачи
+//function thirdTask() {
+for (let i=2; i<=10; i++) {
+	if (i%2===1) continue;
+	console.log(i);
+}
+			
+    
+//}
+
+// Место для четвертой задачи
+
+// Цикл, который нужно переписать:
+
+// for (let i = 2; i <= 16; i++) {
+//     if (i % 2 === 0) {
+//         continue;
+//     } else {
+//         console.log(i);
+//     }
+// }
+
+//function fourthTask() {
+let i=1;
+while (i<16) {
+	i++;
+	if (i % 2 === 0) {
+		continue;
+	} else {
+		console.log(i);
+	}
+}
+
+    
+//}
+
+// Место для пятой задачи
+
+function fifthTask() {
+	const arrayOfNumbers = [];
+	let j = 0;
+	for (let i = 5; i < 11; i++){
+		j++;
+		arrayOfNumbers[j] = i;
+	}
+
+	const arr = [];
+	for (let i=5; i<=10; i++) {
+		arr[i-5] = i;
+	}
+	console.log(arr);
+		
+	
+	// Не трогаем
+	return arrayOfNumbers;
+}
+//-------------------------------------------
