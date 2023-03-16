9df89c4366bf7ceb00c272a41ecb79a5835b93b3
Zahliada VM <waah777@gmail.com>
2023-03-16 15:19:12 +0200

    025_2 end

---
+++ 025/025_1.js
@@ -0,0 +1,35 @@
+console.log('=> js/script.js');
+'use strict';
+
+console.log('исследую область видимости переменной');
+
+let level = 0;
+log(`область ${level}: v`);
+let v1 = 10;
+log(v1);
+{
+	level++;
+	log(`область ${level}: v`);
+	let v1 = 20;
+	log(v1);
+	{
+		level++;
+		log(`область ${level}: v`);
+		let v1 = 30;
+		log(v1);
+		log(`область ${level}: ^`);
+		level--;
+	}
+	log(v1);
+	log(`область ${level}: ^`);
+	level--;
+}
+log(v1);
+log(`область ${level}: ^`);
+
+function log(_val){
+	let s='';
+	for (let i=0; i<level; i++) s += '\t';
+	s += '>';
+	console.log(s, _val);
+}
