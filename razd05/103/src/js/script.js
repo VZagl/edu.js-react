20d2435961ecc0c8e520ba5a2714159b7df55395
Zahliada VM <waah777@gmail.com>
2023-05-18 20:08:39 +0300

    103 end

---
+++ razd05/103/src/js/script.js
@@ -0,0 +1,11 @@
+// import {one, two} from './main';
+// import {one as first, two} from './main';
+import * as data from './main.js';
+import say from './main.js';
+
+// console.log(`${first} and ${two}`);
+console.log(`${data.one} and ${data.two}`);
+console.log('');
+
+// data.sayHi();
+say();
