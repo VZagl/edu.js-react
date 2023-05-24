4c92b2e5c5723e55dd7c0b64e83f63b7bf72e49b
Zahliada VM <waah777@gmail.com>
2023-05-24 20:32:58 +0300

    110 end

---
+++ razd05/110/script.js
@@ -0,0 +1,20 @@
+import $ from 'jquery';
+
+$(document).ready(function(){
+	$('.list-item:first').hover(function(){
+		console.log(this);
+		$(this).toggleClass('active');
+	});
+
+	$('.list-item:eq(2)').on('click', function(){
+		$('.image:even').fadeToggle('slow');
+	});
+
+	$('.list-item:eq(4)').on('click', function(){
+		$('.image:odd').animate({
+			opacity: 'toggle',
+			height: 'toggle'
+		}, 2000);
+	});
+});
+
