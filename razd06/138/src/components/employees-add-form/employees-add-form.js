734d079f3ac7bdbfe7116606282f118ff69c88ed
Zahliada VM <waah777@gmail.com>
2023-06-10 21:04:58 +0300

    138 end

---
+++ razd06/138/src/components/employees-add-form/employees-add-form.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import './employees-add-form.css';
+
+class EmployeesAddForm extends React.Component{
+	constructor(props) {
+		super(props);
+		this.state = {
+			name: '',
+			salary: ''
+		};
+	}
+
+	onValueChange = (e) => {
+		e.target.value = e.target.value.trimStart();
+		this.setState({
+			[e.target.name]: e.target.value
+		});
+	};
+
+	onAddItem = (e) => {
+		e.preventDefault();
+		if(!this.state.name || this.state.name.trim().length < 3 || !this.state.salary) return;
+		this.props.doAddItem({
+			  name: this.state.name.trim()
+			, salary: this.state.salary
+		});
+		this.setState( {name:'', salary: ''} );
+	};
+
+	render () {
+		const {name, salary} = this.state;
+
+		return (
+			<div className="app-add-form">
+				<h3>Добавьте нового сотрудника</h3>
+				<form className="add-form d-flex" onSubmit={this.onAddItem}>
+					<input type="text"
+						className="form-control new-post-label"
+						placeholder="Как его зовут?"
+						name='name'
+						value={name}
+						onChange={this.onValueChange}
+					/>
+					<input type="number"
+						className="form-control new-post-label"
+						placeholder="З/П в $?"
+						name='salary'
+						value={salary}
+						onChange={this.onValueChange}
+					/>
+					<button type="submit" className="btn btn-outline-light">Добавить</button>
+				</form>
+			</div>
+		);
+	};
+}
+
+export default EmployeesAddForm;
