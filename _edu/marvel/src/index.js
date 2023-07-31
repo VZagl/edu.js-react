7a319a13671163f8dce3aa4b755bdef31ed1f916
Zahliada VM <waah777@gmail.com>
2023-07-31 22:01:47 +0300

    171 end

--- _edu/marvel/src/index.js
+++ _edu/marvel/src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import App from './components/app/App';
 
 import './style/style.scss';
 
-const container = document.getElementById('root');
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(
-	// <React.StrictMode>
-	<App />
-	// </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')) //
+	.render(
+		// <React.StrictMode>
+		<App />
+		// </React.StrictMode>
+	);
