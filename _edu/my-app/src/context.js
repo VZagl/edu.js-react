37d924df980c9d25066ef5bdaceafa88f38e8255
Zahliada VM <waah777@gmail.com>
2023-08-08 21:13:49 +0300

    179 end

--- _edu/my-app/src/context.js
+++
@@ -1,9 +0,0 @@
-import { createContext } from 'react';
-
-const dataContext = createContext({
-	mail: '@example.com',
-	text: 'text',
-	forceChangeMail: () => {},
-});
-
-export default dataContext;
