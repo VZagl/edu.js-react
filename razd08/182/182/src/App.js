a681e422b168562fe600fa2f2e75196c4f597125
Zahliada VM <waah777@gmail.com>
2023-08-10 19:26:26 +0300

    182 end

---
+++ razd08/182/182/src/App.js
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import { Container } from 'react-bootstrap';
+import { CSSTransition } from 'react-transition-group';
+
+import './App.css';
+
+const Modal = (props) => {
+	const duration = 500;
+
+	return (
+		<CSSTransition
+			in={props.show}
+			timeout={duration}
+			mountOnEnter
+			unmountOnExit
+			onEnter={() => props.setShowBtn(false)}
+			onExited={() => props.setShowBtn(true)}
+			classNames='modal'
+		>
+			<div className='modal mt-5 d-block'>
+				<div className='modal-dialog'>
+					<div className='modal-content'>
+						<div className='modal-header'>
+							<h5 className='modal-title'>Typical modal window</h5>
+							<button
+								onClick={() => props.onClose(false)}
+								type='button'
+								className='btn-close'
+								aria-label='Close'
+							></button>
+						</div>
+						<div className='modal-body'>
+							<p>Modal body content</p>
+						</div>
+						<div className='modal-footer'>
+							<button
+								onClick={() => props.onClose(false)}
+								type='button'
+								className='btn btn-secondary'
+							>
+								Close
+							</button>
+							<button
+								onClick={() => props.onClose(false)}
+								type='button'
+								className='btn btn-primary'
+							>
+								Save changes
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</CSSTransition>
+	);
+};
+
+function App() {
+	const [showModal, setShowModal] = useState(false);
+	const [showBtn, setShowBtn] = useState(true);
+
+	return (
+		<Container>
+			<Modal show={showModal} onClose={setShowModal} setShowBtn={setShowBtn} />
+			{showBtn && (
+				<button
+					type='button'
+					className='btn btn-warning mt-5'
+					onClick={() => setShowModal(true)}
+				>
+					Open Modal
+				</button>
+			)}
+		</Container>
+	);
+}
+
+export default App;
