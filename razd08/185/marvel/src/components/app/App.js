254add1ba752b0c2b4b2ffc21369b91bd8d7af03
Zahliada VM <waah777@gmail.com>
2023-08-14 16:10:36 +0300

    185 end

---
+++ razd08/185/marvel/src/components/app/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import AppHeader from '../appHeader/AppHeader';
+import Spinner from '../spinner/Spinner';
+
+// import { MainPage, ComicsPage, SingleComicPage } from '../pages';
+const Page404 = React.lazy(() => import('../pages/404'));
+const MainPage = React.lazy(() => import('../pages/MainPage'));
+const ComicsPage = React.lazy(() => import('../pages/ComicsPage'));
+const SinglePage = React.lazy(() => import('../pages/SinglePage'));
+
+const App = () => {
+	return (
+		<Router>
+			<div className='app'>
+				<AppHeader />
+				<main>
+					<React.Suspense fallback={<Spinner />}>
+						<Routes>
+							<Route path='/' element={<MainPage />} />
+
+							<Route path='/comics' element={<ComicsPage />} />
+							<Route
+								path='/comics/:id'
+								element={<SinglePage dataType='comic' />}
+							/>
+							<Route
+								path='/characters/:id'
+								element={<SinglePage dataType='char' />}
+							/>
+
+							<Route path='*' element={<Page404 />} />
+						</Routes>
+					</React.Suspense>
+				</main>
+			</div>
+		</Router>
+	);
+};
+
+export default App;
---
+++ razd08/185/marvel/src/components/charList/CharList.js
@@ -0,0 +1,134 @@
+import React from 'react';
+import { useState, useEffect, useRef } from 'react';
+import useMarvelService from '../../services/MarvelService';
+import ErrorMessage from '../errorMessage/ErrorMessage';
+import PropTypes from 'prop-types';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+
+import './charList.scss';
+import Spinner from '../spinner/Spinner';
+
+const CharList = (props) => {
+	const [charList, setCharList] = useState([]);
+	const [perPage] = useState(9);
+	const [pageLast, setPageLast] = useState(171);
+	const [charEnded, setCharEnded] = useState(false);
+	const [charCurrent, setCharCurrent] = useState(null);
+
+	const { loading, error, getAllCharacters } = useMarvelService();
+	// const itemRefs = useRef([]);
+	const itemRefs = useRef(new Object(null));
+
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	useEffect(() => onRequest(), []);
+
+	const onRequest = () => {
+		if (loading) return;
+
+		getAllCharacters(perPage, pageLast + 1)
+			.then(onCharListLoaded)
+			.catch(onError);
+	};
+
+	const onCharListLoaded = (_newCharList) => {
+		setCharList([...charList, ..._newCharList]);
+		setPageLast(pageLast + 1);
+		setCharEnded(_newCharList.length < perPage);
+	};
+
+	const onError = (e) => console.log('error', e.message);
+
+	const focusOnItem = (_item) => {
+		// console.log('>> CharList > focusOnItem', _item.id);
+		// if (charCurrent && charCurrent.id === _item.id) return;
+
+		setCharCurrent(_item);
+		props.onCharSelected(_item.id);
+		// это делать не обязательно. фокус после клика или табиндекса и так на этом элементе
+		itemRefs.current[_item.id].focus();
+	};
+
+	function renderItems(_charList) {
+		const vContent = _charList.map((_item) => {
+			const { name, thumbnail } = _item;
+			const styleImg =
+				thumbnail.indexOf('image_not_available') === -1
+					? {}
+					: { objectFit: 'contain' };
+			const charClasses = [
+				'char__item',
+				charCurrent && charCurrent.id === _item.id
+					? ' char__item_selected'
+					: null,
+			].join(' ');
+
+			return (
+				<CSSTransition
+					key={_item.id}
+					// nodeRef={nodeRef}
+					in={true}
+					timeout={500}
+					// classNames='page'
+					// unmountOnExit
+					classNames='char__item'
+				>
+					<li
+						ref={(_el) => (itemRefs.current[_item.id] = _el)}
+						className={charClasses}
+						// key={_item.id}
+						tabIndex={0}
+						onClick={() => {
+							focusOnItem(_item);
+						}}
+						onKeyDown={(e) => {
+							if (e.key === ' ' || e.key === 'Enter') {
+								focusOnItem(_item);
+							}
+						}}
+					>
+						<img src={thumbnail} alt={name} style={styleImg} />
+						<div className='char__name'>{name}</div>
+					</li>
+				</CSSTransition>
+			);
+		});
+
+		return (
+			<ul className='char__grid'>
+				<TransitionGroup component={null}>
+					{/**/}
+					{vContent}
+				</TransitionGroup>
+			</ul>
+		);
+	}
+
+	const vError = error ? <ErrorMessage /> : null;
+	const vSpinner = loading ? <Spinner /> : null;
+	const vItems = error ? null : renderItems(charList);
+	const vBtnLoadMore_Style = charEnded ? { display: 'none' } : null;
+
+	return (
+		<div className='char__list'>
+			{vItems}
+			{vError}
+			{vSpinner}
+			{!loading && (
+				<button
+					className='button button__main button__long'
+					onClick={onRequest}
+					disabled={loading}
+					style={vBtnLoadMore_Style}
+				>
+					<div className='inner'>load more</div>
+				</button>
+			)}
+		</div>
+	);
+};
+
+CharList.propTypes = {
+	onCharSelected: PropTypes.func.isRequired,
+};
+
+export default CharList;
