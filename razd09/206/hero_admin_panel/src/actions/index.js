d04dab733908cbeafbedca777c5b48ae2432a0fa
Zahliada VM <waah777@gmail.com>
2023-08-25 12:44:12 +0300

    206 end

---
+++ razd09/206/hero_admin_panel/src/actions/index.js
@@ -0,0 +1,92 @@
+import { createAction } from '@reduxjs/toolkit';
+
+export const fetchHeroes = (request) => (dispatch) => {
+	dispatch(heroesFetching());
+	request('http://localhost:3001/heroes')
+		.then((data) => dispatch(heroesFetched(data)))
+		.catch(() => dispatch(heroesFetchingError()));
+};
+
+export const heroesFetching = createAction('HEROES_FETCHING');
+export const heroesFetched = createAction('HEROES_FETCHED');
+export const heroesFetchingError = createAction('HEROES_FETCHING_ERROR');
+
+export const filtersFetching = createAction('FILTERS_FETCHING');
+export const filtersFetched = createAction('FILTERS_FETCHED');
+export const filtersFetchingError = createAction('FILTERS_FETCHING_ERROR');
+export const activeFilterChanged = createAction('ACTIVE_FILTER_CHANGED');
+
+export const heroCreated = createAction('HERO_CREATED');
+export const heroDeleted = createAction('HERO_DELETED');
+
+/*
+export const heroesFetching = () => {
+	return {
+		type: 'HEROES_FETCHING',
+	};
+};
+
+export const heroesFetched = (heroes) => {
+	return {
+		type: 'HEROES_FETCHED',
+		payload: heroes,
+	};
+};
+
+
+export const heroesFetchingError = () => {
+	return {
+		type: 'HEROES_FETCHING_ERROR',
+	};
+};
+*/
+
+export const fetchFilters = (request) => (dispatch) => {
+	dispatch(filtersFetching());
+	request('http://localhost:3001/filters')
+		.then((data) => dispatch(filtersFetched(data)))
+		.catch(() => dispatch(filtersFetchingError()));
+};
+
+/*
+export const filtersFetching = () => {
+	return {
+		type: 'FILTERS_FETCHING',
+	};
+};
+
+export const filtersFetched = (filters) => {
+	return {
+		type: 'FILTERS_FETCHED',
+		payload: filters,
+	};
+};
+
+export const filtersFetchingError = () => {
+	return {
+		type: 'FILTERS_FETCHING_ERROR',
+	};
+};
+
+export const activeFilterChanged = (filter) => {
+	return {
+		type: 'ACTIVE_FILTER_CHANGED',
+		payload: filter,
+	};
+};
+
+
+export const heroCreated = (hero) => {
+	return {
+		type: 'HERO_CREATED',
+		payload: hero,
+	};
+};
+
+export const heroDeleted = (id) => {
+	return {
+		type: 'HERO_DELETED',
+		payload: id,
+	};
+};
+*/
